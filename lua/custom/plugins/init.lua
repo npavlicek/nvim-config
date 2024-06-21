69377d82b984b1659da6cd84132167758edae444
Niko Pavlicek <npavlicek@users.noreply.github.com>
2024-06-21 03:21:49 -0400

    personal changes

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -1,5 +1,41 @@
--- You can add your own plugins here or in other files in this directory!
---  I promise not to create any merge conflicts in this directory :)
---
--- See the kickstart.nvim README for more information
-return {}
+return {
+	{
+		"ray-x/lsp_signature.nvim",
+		event = "VeryLazy",
+		opts = {},
+		config = function(_, opts)
+			require("lsp_signature").setup(opts)
+		end,
+	},
+	--[[
+	{
+		"rose-pine/neovim",
+		name = "rose-pine",
+		config = function()
+			vim.cmd.colorscheme("rose-pine")
+		end,
+	},
+	--]]
+	{
+		"EdenEast/nightfox.nvim",
+		priority = 1000,
+		config = function()
+			vim.cmd.colorscheme("carbonfox")
+		end,
+	},
+	-- Github Copilot
+	-- { "github/copilot.vim" },
+	-- Sticky headers
+	{
+		"nvim-treesitter/nvim-treesitter-context",
+		after = "nvim-treesitter",
+		config = function()
+			require("treesitter-context").setup()
+		end,
+	},
+	{
+		"ThePrimeagen/harpoon",
+		branch = "harpoon2",
+		dependencies = { "nvim-lua/plenary.nvim" },
+	},
+}
